8d0ee5b755277a71d0b553d9877a3df68fe95f12
pastre <pastre68@gmail.com>
2019-11-25 17:27:47 -0300

    Created basic TaskTimer protocol

--- startupeiros/game/Protocols.swift
+++ startupeiros/game/Protocols.swift
@@ -49,64 +49,64 @@ protocol Levelable {
     func getNextLevelRequirement() -> Int
 }
 
-//
-//
-//class TaskTimer: Timeable {
-//    internal var duration: TimeInterval
-//    internal var _isRunning: Bool
-//    internal var currentTime: TimeInterval
-//    
-//    
-//    init(duration: TimeInterval) {
-//        self.duration = duration
-//        self._isRunning = false
-//        self.currentTime = 0
-//    }
-//    
-//    var timer: Timer?
-//    
-//    func getDuration() -> TimeInterval {
-//        return self.duration
-//    }
-//    
-//    func getCurrentTime() -> TimeInterval {
-//        return self.currentTime
-//    }
-//    
-//    func getRemainingTime() -> TimeInterval {
-//        return self.getDuration() - self.getCurrentTime()
-//    }
-//    
-//    func isDone() -> Bool {
-//        return self.getDuration() == self.getCurrentTime()
-//    }
-//    
-//    func isRunning() -> Bool {
-//        return self._isRunning
-//    }
-//    
-//    func setupTimer() {
-//        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.onTrigger), userInfo: nil, repeats: false)
-//    }
-//    
-//    func run() {
-//        self.runCycle(self.duration)
-//    }
-//    
-//    private func runCycle(_ remainingCyles: TimeInterval) {
-//        if let timer = self.timer {
-//            timer.
-//        } else { return }
-//    }
-//    
-//    func stop() {
-//        <#code#>
-//    }
-//    
-//    
-//    
-//    @objc func onTrigger() {
-//        
-//    }
-//    
-//}
+
+
+class TaskTimer: Timeable {
+    internal var duration: TimeInterval
+    internal var _isRunning: Bool
+    internal var currentTime: TimeInterval
+    
+    
+    init(duration: TimeInterval) {
+        self.duration = duration
+        self._isRunning = false
+        self.currentTime = 0
+    }
+    
+    var timer: Timer?
+    
+    func getDuration() -> TimeInterval {
+        return self.duration
+    }
+    
+    func getCurrentTime() -> TimeInterval {
+        return self.currentTime
+    }
+    
+    func getRemainingTime() -> TimeInterval {
+        return self.getDuration() - self.getCurrentTime()
+    }
+    
+    func isDone() -> Bool {
+        return self.getDuration() == self.getCurrentTime()
+    }
+    
+    func isRunning() -> Bool {
+        return self._isRunning
+    }
+    
+    func setupTimer() {
+        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.onTrigger), userInfo: nil, repeats: false)
+    }
+    
+    func run() {
+        self.runCycle(self.duration)
+    }
+    
+    private func runCycle(_ remainingCyles: TimeInterval) {
+        if let timer = self.timer {
+            timer.
+        } else { return }
+    }
+    
+    func stop() {
+        <#code#>
+    }
+    
+    
+    
+    @objc func onTrigger() {
+        
+    }
+    
+}
