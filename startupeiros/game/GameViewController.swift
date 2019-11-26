058884831f748b63ac9e7572552d236b6062dca0
pastre <pastre68@gmail.com>
2019-11-26 00:23:06 -0300

    Encapsulated Timeable in a class

---
+++ startupeiros/game/GameViewController.swift
@@ -0,0 +1,69 @@
+//
+//  GameViewController.swift
+//  startupeiros
+//
+//  Created by Bruno Pastre on 26/11/19.
+//  Copyright © 2019 Bruno Pastre. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController, TimerDelegate
+{
+    
+    var currentTimer: TaskTimer?
+    func onTrigger() {
+        print("TRIGER")
+    }
+    
+    func onComplete() {
+        print("COMPLETE")
+    }
+    
+    func onInvalidated() {
+        print("INVALIDATED")
+    }
+    
+
+    let button: UIButton = {
+        let button = UIButton()
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("TESTE", for: .normal)
+        button.backgroundColor = .green
+        
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.button.addTarget(self, action: #selector(self.onButton), for: .touchDown)
+        
+        self.view.addSubview(self.button)
+        
+        self.button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
+        self.button.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.7).isActive = true
+        self.button.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.3).isActive = true
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func onButton() {
+        self.currentTimer = TimerFactory.timer()
+        self.currentTimer?.delegate = self
+        self.currentTimer?.run()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
