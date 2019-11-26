12785b1f3ce554a1c2d2f9faedfdd5ce7ee31540
pastre <pastre68@gmail.com>
2019-11-26 00:26:17 -0300

    Separated files

--- startupeiros/game/Protocols.swift
+++ startupeiros/game/Protocols.swift
@@ -8,27 +8,6 @@
 
 import Foundation
 
-protocol TimerDelegate {
-    func onTrigger()
-    func onComplete()
-    func onInvalidated()
-}
-
-protocol Timeable {
-    var duration: TimeInterval { get set }
-    var _isRunning: Bool { get set }
-    var currentTime: TimeInterval { get set }
-    
-    func getDuration() -> TimeInterval
-    func getCurrentTime() -> TimeInterval
-    func getRemainingTime() -> TimeInterval
-    func isDone() -> Bool
-    func isRunning() -> Bool
-    func run()
-    func stop()
-}
-
-
 protocol Lockable {
     func isLocked() -> Bool
     func lock()
