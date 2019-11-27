3c3c6566810567801135ec392bd15c9e3e57d012
Maykon <maykonmeneghel@icloud.com>
2019-11-27 14:39:13 -0300

    Exclude the save job function from DatabaseJob

--- startupeiros/DatabaseJob.swift
+++ startupeiros/DatabaseJob.swift
@@ -22,24 +22,5 @@ class DatabaseJob: Codable{
     var hackerSkills: [DatabaseSkill]
     var hustlerSkills: [DatabaseSkill]
     var hipsterSkills: [DatabaseSkill]
-    
-    func saveGame() {
-        do {
-            let db = Firestore.firestore()
-            let encoder = JSONEncoder()
-            var ref: DocumentReference? = nil
-            let data = try encoder.encode(self)
-            let dict = try JSONSerialization.jsonObject(with: data) as! [String : Any]
-            ref = db.collection("Jobs").addDocument(data: dict) { err in
-                if let err = err {
-                    print("Error adding document: \(err)")
-                } else {
-                    print("Document added with ID: \(ref!.documentID)")
-                }
-            }
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
 }
 
