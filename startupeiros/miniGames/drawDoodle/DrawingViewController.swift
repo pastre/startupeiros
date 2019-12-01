c76dcaa25945ef4d77aff33b63c98445704fbe7a
Maykon <maykonmeneghel@icloud.com>
2019-12-01 00:10:51 -0300

    Code refactoring

--- startupeiros/miniGames/drawDoodle/DrawingViewController.swift
+++ startupeiros/miniGames/drawDoodle/DrawingViewController.swift
@@ -8,31 +8,16 @@
 
 import UIKit
 
-class Canvas: UIView {
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
-        guard let context = UIGraphicsGetCurrentContext() else
-        {return}
-        
-        let startPoint = CGPoint(x: 0, y: 0)
-        let endPoint = CGPoint(x: 100.0, y: 100.0)
-        
-        context.move(to: startPoint)
-        context.addLine(to: endPoint)
-        
-        context.strokePath()
-        //
-    }
-}
-
 class DrawingViewController: UIViewController {
 
     var canvas = Canvas()
     
+    override func loadView() {
+        self.view = canvas
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(canvas)
-        canvas.frame = view.frame
+        canvas.backgroundColor = UIColor.white
     }
 
 }
