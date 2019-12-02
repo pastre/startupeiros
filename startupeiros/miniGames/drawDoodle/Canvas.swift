759ac455072f32079eaf7fa3ced1c83f38315253
Maykon <maykonmeneghel@icloud.com>
2019-12-02 14:09:36 -0300

    Adds stroke different width

--- startupeiros/miniGames/drawDoodle/Canvas.swift
+++ startupeiros/miniGames/drawDoodle/Canvas.swift
@@ -7,34 +7,53 @@
 //
 
 import UIKit
-
+struct Line {
+    var color: UIColor  = .black
+    var stroke: Float = 5.0
+    var points: [CGPoint]
+}
 class Canvas: UIView {
     
-    var lines = [[CGPoint]]()
+    var lines = [Line]()
+    var strokeWidth: Float = 5.0
+    
+    public func setStrokeWidth(width: Float){
+        self.strokeWidth = width
+    }
+    
+    public func undo(){
+        _ = lines.popLast()
+        setNeedsDisplay()
+    }
+    
+    public func clear(){
+        lines.removeAll()
+        setNeedsDisplay()
+    }
     
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         guard let context = UIGraphicsGetCurrentContext() else {return}
         
-        context.setStrokeColor(UIColor.red.cgColor)
-        context.setLineWidth(10)
-        context.setLineCap(.butt)
-        
         lines.forEach { (line) in
-            for (i, p) in line.enumerated(){
-                if i == 0{
+            context.setStrokeColor(line.color.cgColor)
+            context.setLineWidth(CGFloat(line.stroke))
+            context.setLineCap(.round)
+            for (i, p) in line.points.enumerated(){
+              if i == 0{
                     context.move(to: p)
                 }else{
                     context.addLine(to: p)
                 }
             }
+            context.strokePath()
         }
-        context.strokePath()
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("touchBegan")
-        lines.append([CGPoint]())
+        lines.append(Line.init(stroke: strokeWidth, points: []))
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -42,10 +61,16 @@ class Canvas: UIView {
         guard let point = touches.first?.location(in: nil) else {return}
 //        print(point)
         guard var lastLine = lines.popLast() else {return}
-        lastLine.append(point)
+        lastLine.points.append(point)
         lines.append(lastLine)
         
         setNeedsDisplay()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        _ = self.screenshot
+        
+        print("jogar na MobileNet")
     }
     
 }
