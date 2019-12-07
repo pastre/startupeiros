489653f77229e8c705b1cadd789220b43fe94a55
Bruno Pastre <pastre68@gmail.com>
2019-12-07 11:58:50 -0300

    Implemented vote mechanics in the backend

--- startupeiros/controller/meetup/MeetupViewController.swift
+++ startupeiros/controller/meetup/MeetupViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class Vote {
     
@@ -25,6 +26,7 @@ class MeetupViewController: UIViewController, UICollectionViewDataSource, UIColl
     
     var votes: [Vote]! = []
     var jobs: [Job]!
+    var currentSelectedJob: String?
 
     let registerId = "register"
     
@@ -39,11 +41,63 @@ class MeetupViewController: UIViewController, UICollectionViewDataSource, UIColl
         // Do any additional setup after loading the view.
     }
     
+    // MARK: - Firebase functions
+    
     func setupFirebase(){
+        guard let teamId = PlayerFacade.getPlayerTeamId() else { return }
         
+        FirebaseReferenceFactory.meetup(teamId).observe(.value) {
+            (snap) in
+            self.onMeetupUpdated(to: snap)
+        }
+        
+        self.createMeetupIfNeeded()
     }
     
-    // MARK: - Firebase functions
+    func createMeetupIfNeeded(){
+//        guard let teamId = PlayerFacade.getPlayerTeamId() else { return }
+//
+//        FirebaseReferenceFactory.team(teamId).observeSingleEvent(of: .value) { (snap) in
+//            guard let value = snap.value as? NSDictionary else { return }
+//
+//
+//        }
+    }
+    
+    func onMeetupUpdated(to snap: DataSnapshot) {
+        guard let votes = snap.value as? NSDictionary else { return }
+        guard let currentPClass = PlayerFacade.getPlayerClass() else { return }
+        var newVotes: [Vote] = []
+        self.currentSelectedJob = nil
+        for (playerClass, vName) in votes {
+            let pClass = PlayerClass(from: playerClass as! String)
+            let voteName = vName as! String
+            let vote = Vote(name: voteName, whoPicked: pClass)
+            
+            if pClass == currentPClass {
+                self.currentSelectedJob = voteName
+            }
+            
+            newVotes.append(vote)
+        }
+        
+        self.votes = newVotes
+        print("SNAP IS", snap)
+        self.collectionView.reloadData()
+    }
+    
+    func vote(on job: Job){
+        guard let teamId = PlayerFacade.getPlayerTeamId() else { return }
+        guard let playerClass = PlayerFacade.getPlayerClass() else { return }
+    FirebaseReferenceFactory.meetup(teamId).child(playerClass.rawValue).setValue(job.getName())
+    }
+    
+    func clearVote() {
+        guard let teamId = PlayerFacade.getPlayerTeamId() else { return }
+        guard let playerClass = PlayerFacade.getPlayerClass() else { return }
+    FirebaseReferenceFactory.meetup(teamId).child(playerClass.rawValue).setValue(nil)
+
+    }
     
     // MARK: - CollectionView Data source
     
@@ -59,14 +113,35 @@ class MeetupViewController: UIViewController, UICollectionViewDataSource, UIColl
         
         let job = self.jobs[indexPath.item]
         
-        cell.setup(jobName: job.getName(), voteCount: 0)
+        let voteCount = self.votes.filter { (vote) -> Bool in
+            vote.name == job.getName()
+        }.count
+        
+        cell.setup(jobName: job.getName(), voteCount: voteCount)
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let job = self.jobs[indexPath.item]
+        
+        if job.getName() == self.currentSelectedJob {
+            self.clearVote()
+            return
+        }
+        
+        self.vote(on: job)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let size = collectionView.frame.size
         
+        let job = self.jobs[indexPath.item]
+        
+        if job.getName() == self.currentSelectedJob {
+            return CGSize(width: size.width, height: size.height * 0.4)
+            
+        }
         return CGSize(width: size.width * 0.9, height: size.height * 0.3)
     }
     
