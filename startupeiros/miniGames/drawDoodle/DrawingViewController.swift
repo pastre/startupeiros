abfbd77204686c3917186d65e39291f1b34a8a56
Bruno Pastre <pastre68@gmail.com>
2019-12-08 15:29:52 -0300

    Added canvas methods

--- startupeiros/miniGames/drawDoodle/DrawingViewController.swift
+++ startupeiros/miniGames/drawDoodle/DrawingViewController.swift
@@ -21,7 +21,102 @@ class DrawingViewController: UIViewController {
     private var currentStroke: CGMutablePath? { return strokes.last }
     private var imageViewSize: CGSize { return imageView.frame.size }
     private let classifier = Doodle_1()
-    // END dd_new_vars
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        undoButton.disable()
+    }
+ 
+    // MARK: - Touches overrides
+    override func touchesBegan(_ touches: Set<UITouch>,
+           with event: UIEvent?) {
 
-     
+           guard let touch = touches.first else { return }
+           
+           let newStroke = CGMutablePath()
+           newStroke.move(to: touch.location(in: imageView))
+           strokes.append(newStroke)
+           refresh()
+       }
+    
+       override func touchesMoved(_ touches: Set<UITouch>,
+           with event: UIEvent?) {
+
+           guard let touch = touches.first,
+               let currentStroke = self.currentStroke else {
+                   return
+           }
+           
+           currentStroke.addLine(to: touch.location(in: imageView))
+           refresh()
+       }
+    
+       override func touchesEnded(_ touches: Set<UITouch>,
+           with event: UIEvent?) {
+
+           guard let touch = touches.first,
+               let currentStroke = self.currentStroke else {
+                   return
+           }
+           
+           currentStroke.addLine(to: touch.location(in: imageView))
+           refresh()
+       }
+    
+    // MARK: - Canvas related functions
+    
+    func undo() {
+        let _ = strokes.removeLast()
+        refresh()
+    }
+    
+    func clear() {
+        strokes = []
+        classification = nil
+        refresh()
+    }
+    
+    func refresh() {
+        if self.strokes.isEmpty { self.imageView.image = nil }
+        
+        let drawing = makeImage(from: self.strokes)
+        self.imageView.image = drawing
+        
+        if classification != nil {
+            undoButton.disable()
+            clearButton.enable()
+        } else if !strokes.isEmpty {
+            undoButton.enable()
+            clearButton.enable()
+        } else {
+            undoButton.disable()
+            clearButton.disable()
+        }
+        
+        classLabel.text = classification ?? ""
+    }
+    
+    
+    func makeImage(from strokes: [CGMutablePath]) -> UIImage? {
+        let image = CGContext.create(size: imageViewSize) { context in
+            context.setStrokeColor(UIColor.black.cgColor)
+            context.setLineWidth(8.0)
+            context.setLineJoin(.round)
+            context.setLineCap(.round)
+            
+            for stroke in strokes {
+                context.beginPath()
+                context.addPath(stroke)
+                context.strokePath()
+            }
+        }
+
+        return image
+    }
+
+    func classify() {
+        
+    }
+
 }
