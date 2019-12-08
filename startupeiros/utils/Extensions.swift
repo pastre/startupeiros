9059a00589a944fc0d4b30cba99c9c5ac698fe90
Bruno Pastre <pastre68@gmail.com>
2019-12-08 15:26:11 -0300

    Added extensions

--- startupeiros/utils/Extensions.swift
+++ startupeiros/utils/Extensions.swift
@@ -35,3 +35,45 @@ extension Double: Coin {
     }
     
 }
+
+extension CGContext {
+    static func create(size: CGSize,
+        action: (inout CGContext) -> ()) -> UIImage? {
+
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        
+        guard var context = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+
+        action(&context)
+
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+
+        UIGraphicsEndImageContext()
+        
+        return result
+    }
+}
+// END dd_new_extensions_cgcontext
+
+// BEGIN dd_new_extensions_uibutton
+extension UIButton {
+    func enable() {
+        self.isEnabled = true
+        self.backgroundColor = UIColor.systemBlue
+    }
+    
+    func disable() {
+        self.isEnabled = false
+        self.backgroundColor = UIColor.lightGray
+    }
+}
+// END dd_new_extensions_uibutton
+
+// BEGIN dd_new_extensions_uibarbuttonitem
+extension UIBarButtonItem {
+    func enable() { self.isEnabled = true }
+    func disable() { self.isEnabled = false }
+}
+// END dd_new_extensions_uibarbuttonitem
