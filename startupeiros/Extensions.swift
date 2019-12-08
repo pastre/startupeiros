f749e64d2bba0c416714341fc6b703483cfb00c7
Maykon <maykonmeneghel@icloud.com>
2019-12-08 19:31:38 -0300

    Add a extension to apply sketch shadow in the CALayer class

--- startupeiros/Extensions.swift
+++ startupeiros/Extensions.swift
@@ -35,3 +35,26 @@ extension Double: Coin {
     }
     
 }
+
+extension CALayer {
+  func applySketchShadow(
+    color: UIColor = .black,
+    alpha: Float = 0.5,
+    x: CGFloat = 0,
+    y: CGFloat = -2,
+    blur: CGFloat = 22.0,
+    spread: CGFloat = 0)
+  {
+    shadowColor = color.cgColor
+    shadowOpacity = alpha
+    shadowOffset = CGSize(width: x, height: y)
+    shadowRadius = blur / 2.0
+    if spread == 0 {
+      shadowPath = nil
+    } else {
+      let dx = -spread
+      let rect = bounds.insetBy(dx: dx, dy: dx)
+      shadowPath = UIBezierPath(rect: rect).cgPath
+    }
+  }
+}
