278ffaf3448aae00b0aa7e1504a617387a1e11cb
Bruno Pastre <pastre68@gmail.com>
2019-12-10 02:03:34 -0300

    Removed debug data

--- startupeiros/miniGames/flappyGame/FlappyGameViewController.swift
+++ startupeiros/miniGames/flappyGame/FlappyGameViewController.swift
@@ -40,8 +40,8 @@ class FlappyGameViewController: UIViewController {
         if let scene = FlappyGameScene.unarchiveFromFile("GameScene") as? FlappyGameScene {
             // Configure the view.
             let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
+//            skView.showsFPS = true
+//            skView.showsNodeCount = true
             
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
